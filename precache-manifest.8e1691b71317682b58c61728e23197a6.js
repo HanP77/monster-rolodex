0bc1245883debec551af3c507b6a88353bc7b7be
HanP77 <dev@loicaublet@com>
2020-03-16 17:26:50 +0100

    Updates

---
+++ precache-manifest.8e1691b71317682b58c61728e23197a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbcff31cc1e4a621e1d9331bc2bd9a61",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "13970e9706abcdf62f37",
+    "url": "/monster-rolodex/static/css/main.8f248ad4.chunk.css"
+  },
+  {
+    "revision": "5bc7c05f66c3ef831429",
+    "url": "/monster-rolodex/static/js/2.df32befe.chunk.js"
+  },
+  {
+    "revision": "13970e9706abcdf62f37",
+    "url": "/monster-rolodex/static/js/main.c28bfeb3.chunk.js"
+  },
+  {
+    "revision": "a743169f6b5b3692edaa",
+    "url": "/monster-rolodex/static/js/runtime-main.bcf29e65.js"
+  }
+]);
